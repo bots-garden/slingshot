a3f5a31e0da453dc13e50fc65e6f441ae30bb134
k33g <ph.charriere@gmail.com>
2023-09-16 15:59:00 +0200

    💾 Saved.

--- examples-pdk/go/10-pdk-api/main.go
+++ examples-pdk/go/10-pdk-api/main.go
@@ -2,13 +2,22 @@ package main
 
 import (
 	slingshot "github.com/bots-garden/slingshot/go-pdk"
+	"github.com/extism/go-pdk"
 )
 
 func helloHandler(argHandler []byte) []byte {
 	input := string(argHandler)
 	slingshot.Print("👋 hello world 🌍 " + string(input))
-	
+
 	slingshot.Log("🙂 have a nice day 🏖️")
+
+	pdk.Log(pdk.LogInfo, "😀😃😄")
+
+	firstName, _ := pdk.GetConfig("firstName")
+	lastName, _ := pdk.GetConfig("lastName")
+
+	pdk.Log(pdk.LogInfo, firstName)
+	pdk.Log(pdk.LogInfo, lastName)
 
 	return []byte("👋 Hello World 🌍")
 }
@@ -20,6 +29,7 @@ func callHandler() {
 }
 
 func main() {}
+
 /*
     ./slingshot run --wasm=./print.wasm \
 	--handler=callHandler \
