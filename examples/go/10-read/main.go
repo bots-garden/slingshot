3075fd65cae7f2f2ae02fae8e586dd35e28a35cf
k33g <ph.charriere@gmail.com>
2023-09-17 14:09:49 +0200

    🛟 Updated.

---
+++ examples/go/10-read/main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"errors"
+
+	"github.com/extism/go-pdk"
+	"github.com/valyala/fastjson"
+)
+
+//export hostPrint
+func hostPrint(offset uint64) uint64
+
+func Print(text string) {
+	memoryText := pdk.AllocateString(text)
+	hostPrint(memoryText.Offset())
+}
+
+var parser = fastjson.Parser{}
+
+// GetJsonFromBytes
+/*
+	Convert a buffer (`[]byte`) into a JSON value
+*/
+func GetJsonFromBytes(buffer []byte) (*fastjson.Value, error) {
+	return parser.ParseBytes(buffer)
+}
+
+//export hostReadFile
+func hostReadFile(offset uint64) uint64
+
+func ReadFile(filePath string) (string, error) {
+	// Copy the string value to the shared memory
+	arguments := pdk.AllocateString(filePath)
+
+	// Call host function with the offset of the arguments
+	offset := hostReadFile(arguments.Offset())
+
+	// Get result from the shared memory
+	memoryResult := pdk.FindMemory(offset)
+	buffResult := make([]byte, memoryResult.Length())
+	memoryResult.Load(buffResult)
+	JSONData, err := GetJsonFromBytes(buffResult)
+	if err != nil {
+		return "", err
+	}
+	if len(JSONData.GetStringBytes("failure")) == 0 {
+		return string(JSONData.GetStringBytes("success")), nil
+	} else {
+		return "", errors.New(string(JSONData.GetStringBytes("failure")))
+	}
+
+}
+
+//export hello
+func hello() uint64 {
+
+	content, err := ReadFile("./hello.txt")
+	if err != nil {
+		Print("😡 " + err.Error())
+	} else {
+		Print(content)
+	}
+
+	return 0
+}
+
+func main() {}
---
+++ examples/go/11-write/main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"encoding/base64"
+	"errors"
+
+	"github.com/extism/go-pdk"
+	"github.com/valyala/fastjson"
+)
+
+//export hostPrint
+func hostPrint(offset uint64) uint64
+
+func Print(text string) {
+	memoryText := pdk.AllocateString(text)
+	hostPrint(memoryText.Offset())
+}
+
+var parser = fastjson.Parser{}
+
+// GetJsonFromBytes
+/*
+	Convert a buffer (`[]byte`) into a JSON value
+*/
+func GetJsonFromBytes(buffer []byte) (*fastjson.Value, error) {
+	return parser.ParseBytes(buffer)
+}
+
+//export hostWriteFile
+func hostWriteFile(offset uint64) uint64
+
+func WriteFile(filePath string, contentFile string) error {
+
+	content := base64.StdEncoding.EncodeToString([]byte(contentFile))
+
+	jsonStrArguments := `{"path":"` + filePath + `","content":"` + content + `"}`
+
+	// Copy the string value to the shared memory
+	arguments := pdk.AllocateString(jsonStrArguments)
+
+	// Call the host function with Json string argument
+	offset := hostWriteFile(arguments.Offset())
+
+	// Get result from the shared memory
+	memoryResult := pdk.FindMemory(offset)
+	buffResult := make([]byte, memoryResult.Length())
+	memoryResult.Load(buffResult)
+	JSONData, err := GetJsonFromBytes(buffResult)
+
+	if err != nil {
+		return err
+	}
+	if len(JSONData.GetStringBytes("failure")) == 0 {
+		return nil
+	} else {
+		return errors.New(string(JSONData.GetStringBytes("failure")))
+	}
+}
+
+//export hello
+func hello() uint64 {
+
+	text := `
+	<html>
+	  <h1>"Hello World!!!"</h1>
+	</html>
+	`
+
+	err := WriteFile("./index.html", text)
+	if err != nil {
+		Print("😡 " + err.Error())
+	}
+
+	return 0
+}
+
+func main() {}
